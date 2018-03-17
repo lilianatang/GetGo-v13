050bef128a23f1b9067ef533ac8163b82fdba60f
lilianatang <lilianatang@DESKTOP-KNE0QIR.localdomain>
2018-03-17 02:13:25 -0600

    integrate the whole login system to the project with multiple modules

---
+++ Android_project/app/src/main/java/com/macewan/getgo/getgo_v13/login/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.macewan.getgo.getgo_v13.login.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://10.55.70.30/GetGo-v13/php_scripts/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://10.55.70.30/GetGo-v13/php_scripts/register.php";
+}
