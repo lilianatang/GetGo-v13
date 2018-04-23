4e2e25aa4f8b79ab5ef4bcaad97b1a34a8f3fc67
lilianatang <liliana.quyen.tang@gmail.com>
2018-04-22 19:43:02 -0600

    working version

--- GetGo-now/app/src/main/java/com/macewan/getgo/getgo_now/app/AppConfig.java
+++ GetGo-now/app/src/main/java/com/macewan/getgo/getgo_now/app/AppConfig.java
@@ -4,29 +4,29 @@ public class AppConfig {
 	// Server user login url
 
 
-	public static String localhost  = "216.250.120.68";
+	public static String localhost  = "siham.ca/android_login_api";
   
-	public static String URL_LOGIN = "http://"+ localhost + "/android_login_api/login.php";
+	public static String URL_LOGIN = "http://"+ localhost + "/login.php";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://"+ localhost +  "/android_login_api/register.php";
+	public static String URL_REGISTER = "http://"+ localhost +  "/register.php";
 
 	// Department url
-	public static String URL_DEPARTMENT = "http://"+ localhost  + "/android_login_api/db_department.php";
+	public static String URL_DEPARTMENT = "http://"+ localhost  + "/db_department.php";
 
 	// Conditions url
-	public static String URL_CONDITION = "http://"+ localhost + "/android_login_api/get_conditions.php";
+	public static String URL_CONDITION = "http://"+ localhost + "/get_conditions.php";
 
 	// Condition Links url
-	public static String URL_CONDITION_LINK = "http://"+ localhost + "/android_login_api/get_condition_links.php";
+	public static String URL_CONDITION_LINK = "http://"+ localhost + "/get_condition_links.php";
 
 	//Groups url
-	public static String URL_GROUP = "http://"+ localhost + "/android_login_api/get_groups.php";
+	public static String URL_GROUP = "http://"+ localhost + "/get_groups.php";
 
 	//Courses url
-	public static String URL_COURSE = "http://"+ localhost + "/android_login_api/get_all_courses.php";
+	public static String URL_COURSE = "http://"+ localhost + "/get_all_courses.php";
 
 	//Courses url
-	public static String URL_INSTITUTION = "http://"+ localhost +"/android_login_api/get_institutions.php";
+	public static String URL_INSTITUTION = "http://"+ localhost +"/get_institutions.php";
 
 }
